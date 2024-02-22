84f896b49029ecaaabefba6ca943ce83c2f7b08f
MomenMustafa45 <momenkara2@gmail.com>
2024-02-22 13:02:15 +0200

    update

--- src/app/ui-components/circle-animation/CircleAnimation.tsx
+++ src/app/ui-components/circle-animation/CircleAnimation.tsx
@@ -1,6 +1,10 @@
 import logoLogin from "../../../_metronic/assets/icons/login-logo-pic.png";
 import React from "react";
 import "./circle-animation.css";
+import imgAvatar01 from "../../../_metronic/assets/avatar/300-1.jpg";
+import imgAvatar02 from "../../../_metronic/assets/avatar/300-3.jpg";
+import imgAvatar03 from "../../../_metronic/assets/avatar/300-12.jpg";
+import imgAvatar04 from "../../../_metronic/assets/avatar/300-4.jpg";
 
 export const CircleAnimation = React.memo(() => {
   console.log("circle rendered");
@@ -8,11 +12,23 @@ export const CircleAnimation = React.memo(() => {
   return (
     <>
       <div className="icon-container">
-        <span className="turning-circle"></span>
-        <span className="turning-circle"></span>
         <span className="turning-circle">
-          <p className="ball"></p>
-          <p className="ball"></p>
+          <span className="ball-img">
+            <img src={imgAvatar01} alt="" />
+          </span>
+        </span>
+        <span className="turning-circle">
+          <span className="ball-img">
+            <img src={imgAvatar02} alt="" />
+          </span>
+        </span>
+        <span className="turning-circle">
+          <span className="ball-img">
+            <img src={imgAvatar03} alt="" />
+          </span>
+          <span className="ball-img">
+            <img src={imgAvatar04} alt="" />
+          </span>
         </span>
         <span className="turning-circle"></span>
         <span className="stroke-circle"></span>
