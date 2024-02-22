cbc5df85f48b26a82d9c70612be01479449ba623
MomenMustafa45 <momenkara2@gmail.com>
2024-02-22 13:11:50 +0200

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
-import {Suspense} from 'react'
-import {Outlet} from 'react-router-dom'
-import {I18nProvider} from '../_metronic/i18n/i18nProvider'
-import {LayoutProvider, LayoutSplashScreen} from '../_metronic/layout/core'
-import {MasterInit} from '../_metronic/layout/MasterInit'
-import {AuthInit} from './modules/auth'
-import {ThemeModeProvider} from '../_metronic/partials/layout/theme-mode/ThemeModeProvider'
+import { Suspense, useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { I18nProvider } from "../_metronic/i18n/i18nProvider";
+import { LayoutProvider, LayoutSplashScreen } from "../_metronic/layout/core";
+import { MasterInit } from "../_metronic/layout/MasterInit";
+import { AuthInit } from "./modules/auth";
+import { ThemeModeProvider } from "../_metronic/partials/layout/theme-mode/ThemeModeProvider";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
     <Suspense fallback={<LayoutSplashScreen />}>
       <I18nProvider>
@@ -20,7 +25,7 @@ const App = () => {
         </LayoutProvider>
       </I18nProvider>
     </Suspense>
-  )
-}
+  );
+};
 
-export {App}
+export { App };
